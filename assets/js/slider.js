a4dd7678edb8ea32f20bf1f4e4e95fc37f7aebef
Isaac <isaac.orija@gmail.com>
2021-01-28 14:55:22 +0100

    isaacs changes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,31 +1,31 @@
 const images = [
-  {"item":"assets/images/starks-records.jpg"},
-  {"item":"assets/images/stark-image002.jpeg"},
-  {"item":"assets/images/strakstech002.jpg"}
- ]
+  { "item": "assets/images/corridor.png" },
+  { "item": "assets/images/studio-1.jpg" },
+  { "item": "assets/images/home-banner.png" }
+]
 
-    var i = 0;
-    function iterate() {
+var i = 0;
+function iterate() {
 
-    if ( i >= images.length) {
-        i = 0;
-    }
-      // console.log(images[i].item)
-      // console.log(document.getElementById('Image'))
-      document.getElementById('Image').style.background="linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url("+images[i].item+") no-repeat";
-      i++;
-    }
-  setInterval( iterate, 5000 );
-
-   i = 0;
-  const next = () => {
-    i = i + 1;
-    i = i % images.length;
-    document.getElementById('Image').style.background="linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url("+images[i].item+") no-repeat";
+  if (i >= images.length) {
+    i = 0;
   }
- const prev = () => {
+  // console.log(images[i].item)
+  // console.log(document.getElementById('Image'))
+  document.getElementById('Image').style.background = "linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url(" + images[i].item + ") no-repeat";
+  i++;
+}
+setInterval(iterate, 5000);
+
+i = 0;
+const next = () => {
+  i = i + 1;
+  i = i % images.length;
+  document.getElementById('Image').style.background = "linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url(" + images[i].item + ") no-repeat";
+}
+const prev = () => {
   i = i - 1;
   i = i % images.length;
-  document.getElementById('Image').style.background="linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url("+images[i].item+") no-repeat";
+  document.getElementById('Image').style.background = "linear-gradient(180deg, rgba(48, 39, 66, 0.863) 0%, rgba(38, 27, 58, 0.719) 100%),  url(" + images[i].item + ") no-repeat";
 
 }
